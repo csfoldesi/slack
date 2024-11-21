19532923f88fa9de2c0bacce63708b40784dd1be
Csaba Földesi <csabus@gmail.com>
2024-11-21 22:09:54 +0100

    Get current user info added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
+import { UserButton } from "@/features/auth/components/user-button";
+import { useCreteWorkspaceModal } from "@/features/workspaces/storte/use-create-workspace-modal";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useEffect, useMemo } from "react";
 
 export default function Home() {
-  const {signOut} = useAuthActions();
-  
-  return (
-    <div>
-      Logged in!
-      <Button onClick={() =>{signOut() }}>
-        SIgn out
-      </Button>
-    </div>
-  );
+  const [open, setOpen] = useCreteWorkspaceModal();
+  const { data, isLoading } = useGetWorkspaces();
+
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+    if (workspaceId) {
+      console.log("Redirect to workspace");
+    } else if (!open) {
+      setOpen(true);
+    }
+  }, [isLoading, open, setOpen, workspaceId]);
+
+  return <UserButton />;
 }
