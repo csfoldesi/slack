0e631dcfe763e58a236cedc9a581075c4aa69c26
Csaba Földesi <csabus@gmail.com>
2024-11-26 23:30:33 +0100

    SidebarItem added

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,8 +1,9 @@
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { AlertTriangle, Loader } from "lucide-react";
+import { AlertTriangle, Loader, MessageSquareTextIcon, SendHorizonalIcon } from "lucide-react";
 import { WorkspaceHeader } from "./workspace-header";
+import { SidebarItem } from "./sidebar-item";
 
 export const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
@@ -29,6 +30,10 @@ export const WorkspaceSidebar = () => {
   return (
     <div className="flex flex-col bg-[#5e2c5f] h-full">
       <WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"} />
+      <div className="flex flex-col px-2 mt-3">
+        <SidebarItem label="Threads" icon={MessageSquareTextIcon} id="threads" />
+        <SidebarItem label="Drafts & Sent" icon={SendHorizonalIcon} id="drafts" />
+      </div>
     </div>
   );
 };
